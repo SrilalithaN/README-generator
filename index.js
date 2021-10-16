953c460269a78093019c7995c75fc8aaae4518b4
Sri <srilalitha.ganti@gmail.com>
2021-10-16 21:27:54 +1100

    added questions to index.js file

--- index.js
+++ index.js
@@ -1,8 +1,82 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Please enter the title of the Project(required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter a title for the project to proceed!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a description of the project(Required)",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("Please enter a brief description of project to proceed!");
+        return false;
+      }
+    },
+  },
+
+  {
+    type: "input",
+    name: "installation",
+    message: "Please describe the installation if any",
+  },
+
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use this project?",
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log("Please enter the usage of the Project to proceed!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message:
+      "Please provide guidelines for other developers to contribute to this project?",
+  },
+
+  {
+    type: "input",
+    name: "tests",
+    message: "Please provide the test instructions if any ",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose the appropriate license for this project",
+    choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+  },
+  {
+    type: "input",
+    name: "github",
+    messsage: "Please enter yout Github username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email address",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
